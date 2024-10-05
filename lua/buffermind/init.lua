2715ec1307a8d630f53564ca5b608f4214bff15f
Dat Nguyen <datnguyen1215@gmail.com>
2024-10-04 21:43:57 -0700

    update some functions and reset chat.

--- lua/buffermind/init.lua
+++ lua/buffermind/init.lua
@@ -1,12 +1,31 @@
--- ~/.config/nvim/lua/llm_chat/init.lua
-
-local chat = require("llm_chat.chat")
-local config = require("llm_chat.config")
+local chat = require("buffermind.chat")
+local config = require("buffermind.config")
 
 config.setup()
 
 -- Set up commands and key mappings
-vim.api.nvim_create_user_command("ChatOpen", chat.open_chat_buffer, {})
+vim.api.nvim_create_user_command("ChatReset", chat.reset_chat_buffer, {})
+
+vim.api.nvim_create_user_command("ChatOpen", function()
+	local chat_buf = chat.find_chat_buffer()
+	-- print error logs if chat_buf is nil
+	print("Chat buffer not found")
+
+	if chat_buf then
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local buf = vim.api.nvim_win_get_buf(win)
+			if buf == chat_buf then
+				vim.api.nvim_set_current_win(win)
+				return
+			end
+		end
+		vim.cmd("vsplit")
+		vim.api.nvim_win_set_buf(0, chat_buf)
+	else
+		chat.open_chat_buffer()
+	end
+end, {})
 
 -- Key mapping for <leader>chc to open chat
 vim.api.nvim_set_keymap("n", "<leader>chc", "<cmd>ChatOpen<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>chr", "<cmd>ChatReset<CR>", { noremap = true, silent = true })
